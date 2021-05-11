7092b3dc2f3f6675965b008331fa58640f201006
Orcha02 <orcha2002@gmail.com>
2021-05-11 17:08:49 +0000

    Split c and python languages

---
+++ headers.h
@@ -0,0 +1,7 @@
+#ifndef _HEADERS_H_
+#define _HEADERS_H_
+
+int clear_bit(unsigned long int *n, unsigned int index);
+char *_strncpy(char *dest, char *src, int n);
+int delete_nodeint_at_index(listint_t **head, unsigned int index);
+#endif
