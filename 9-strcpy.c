d1b79a9e175f1c19bc0f98fdf488533f3f12e605
Orcha02 <orcha2002@gmail.com>
2021-05-31 21:34:30 +0000

    Strcopy

---
+++ 9-strcpy.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (*src != '\0')
+	{
+		*dest = *src;
+		src++;
+		dest++;
+		i++;
+	}
+	*dest = '\0';
+	dest = dest - i;
+	return (dest);
+}
